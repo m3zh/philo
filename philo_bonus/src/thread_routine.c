834b8945dbdac637b1e81caef00c8e857fe1acff
Milena Lazzaretti <mlazzare@student.s19.be>
2021-11-02 14:30:09 +0100

    philo

---
+++ philo_bonus/src/thread_routine.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread_routine.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 13:16:12 by mlazzare          #+#    #+#             */
+/*   Updated: 2021/11/02 14:03:25 by mlazzare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+int	someone_died(long int now, t_philo *p)
+{
+	print_routine(now, p, DIE);
+	p->params->over = 1;
+	p->dead = 1;
+	pthread_mutex_unlock(p->left_fork);
+	pthread_mutex_unlock(p->right_fork);
+	return (1);
+}
+
+int	check_death(t_philo *p)
+{
+	long int	now;
+
+	now = time_now(p) - p->last_meal;
+	pthread_mutex_lock(p->params->death);
+	if (p->dead || now > p->params->time2die)
+	{
+		now = time_now(p) - p->thread_start;
+		pthread_mutex_unlock(p->params->death);
+		return (someone_died(now, p));
+	}
+	pthread_mutex_unlock(p->params->death);
+	return (0);
+}
+
+int	ft_sleep_and_think(t_philo *p)
+{
+	long int	now;
+
+	now = time_now(p);
+	while (!p->params->over && (now - p->last_meal) < p->params->time2sleep)
+	{
+		if (now - p->last_meal > p->params->time2die)
+			return (someone_died(now - p->last_meal, p));
+		now = time_now(p);
+	}
+	print_routine(now - p->thread_start, p, SLEEP);
+	print_routine(now - p->thread_start, p, THINK);
+	if (!p->params->over && (now - p->last_meal) > p->params->time2die)
+		return (someone_died(now - p->thread_start, p));
+	return (0);
+}
+
+int	ft_eat(t_philo *p)
+{
+	pthread_mutex_lock(p->left_fork);
+	if (print_routine(time_now(p) - p->thread_start, p, FORK))
+	{
+		pthread_mutex_unlock(p->left_fork);
+		return (1);
+	}
+	pthread_mutex_lock(p->right_fork);
+	if (print_routine(time_now(p) - p->thread_start, p, FORK))
+	{
+		pthread_mutex_unlock(p->left_fork);
+		pthread_mutex_unlock(p->right_fork);
+		return (1);
+	}
+	if (print_routine(time_now(p) - p->thread_start, p, EAT))
+	{
+		pthread_mutex_unlock(p->left_fork);
+		pthread_mutex_unlock(p->right_fork);
+		return (1);
+	}
+	p->last_meal = time_now(p);
+	p->iter_num++;
+	pthread_mutex_unlock(p->left_fork);
+	pthread_mutex_unlock(p->right_fork);
+	return (0);
+}
+
+void	*thread_routine(void *job)
+{
+	int		starved;
+	t_philo	*philo;
+
+	starved = 0;
+	philo = (t_philo *)job;
+	if (philo->id & 1)
+		usleep(150);
+	philo->thread_start = time_now(philo);
+	philo->last_meal = philo->thread_start;
+	while (!philo->dead && !philo->params->over)
+	{
+		if (!starved)
+			starved = ft_eat(philo);
+		if (!starved && !philo->dead && !philo->params->over)
+			ft_sleep_and_think(philo);
+	}
+	return (NULL);
+}
---
+++ philo_bonus/src/utils.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 15:32:10 by mlazzare          #+#    #+#             */
+/*   Updated: 2021/11/02 14:05:04 by mlazzare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+int	error_msg(char *s, t_params *par, t_philo *p, int malloc)
+{
+	int	i;
+
+	i = -1;
+	if (malloc)
+	{
+		free(par->death);
+		free(par->fork);
+		if (malloc == 2)
+			while (++i < par->num)
+				free(&p[i]);
+	}		
+	return (write(STDERR_FILENO, s, ft_strlen(s)));
+}
+
+int	print_routine(long int now, t_philo *p, char *action)
+{
+	pthread_mutex_lock(p->params->death);
+	if (p->dead)
+	{
+		pthread_mutex_unlock(p->params->death);
+		return (1);
+	}
+	printf("[ %ld ms ] Philosopher %d %s\n", now, p->id, action);
+	pthread_mutex_unlock(p->params->death);
+	return (0);
+}
