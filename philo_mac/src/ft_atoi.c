3b73942b0bff4d9536e05734baad90bba0fdef3c
mlazzare <mlazzare@student.s19.be>
2021-11-05 13:38:52 +0100

    philo

---
+++ philo_mac/src/ft_atoi.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/10 08:05:31 by mlazzare          #+#    #+#             */
+/*   Updated: 2021/11/02 09:49:07 by mlazzare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+static int	ft_isnumber(const char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s[i] && (s[i] == '-' || s[i] == '+'))
+		i++;
+	while (s[i])
+	{
+		if (!(s[i] >= '0' && s[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	check_sign(char c)
+{
+	if (c == '-')
+		return (-1);
+	return (1);
+}
+
+static int	check_overflow(int sign)
+{
+	if (sign == 1)
+		return (-1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int						i;
+	int						sign;
+	unsigned long long int	n;
+
+	i = 0;
+	n = 0;
+	if (!ft_isnumber(str))
+		return (-1);
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+		sign = check_sign(str[i++]);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (n >= LONG_MAX)
+		{
+			n = check_overflow(sign);
+			break ;
+		}
+		n = n * 10 + (str[i++] - '0');
+	}
+	return (sign * (int)n);
+}
---
+++ philo_mac/src/thread_routine.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread_routine.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 13:16:12 by mlazzare          #+#    #+#             */
+/*   Updated: 2021/11/05 12:37:50 by mlazzare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+static int	someone_died(long int now, t_philo *p, int unlock, int print)
+{
+	if (print)
+	{
+		print_routine(now, p, DIE);
+		p->params->over = 1;
+		p->dead = 1;
+	}
+	if (unlock)
+	{
+		pthread_mutex_unlock(p->left_fork);
+		pthread_mutex_unlock(p->right_fork);
+	}
+	return (1);
+}
+
+int	check_death(t_philo *p)
+{
+	long int	now;
+
+	now = time_now(p) - p->last_meal;
+	pthread_mutex_lock(p->params->death);
+	if (now > p->params->time2die)
+	{
+		now = time_now(p) - p->thread_start;
+		pthread_mutex_unlock(p->params->death);
+		return (someone_died(now, p, 0, 1));
+	}
+	pthread_mutex_unlock(p->params->death);
+	return (0);
+}
+
+int	ft_sleep_and_think(t_philo *p)
+{
+	long int	now;
+
+	if (p->dead || p->params->over)
+		return (1);
+	ft_usleep(p->params->time2sleep);
+	if (p->dead || p->params->over)
+		return (1);
+	now = time_now(p);
+	print_routine(now - p->thread_start, p, SLEEP);
+	print_routine(now - p->thread_start, p, THINK);
+	if (p->dead || p->params->over)
+		return (1);
+	if (now - p->last_meal > p->params->time2die)
+		return (someone_died(now - p->thread_start, p, 0, 1));
+	return (0);
+}
+
+int	ft_eat(t_philo *p)
+{
+	pthread_mutex_lock(p->left_fork);
+	if (print_routine(time_now(p) - p->thread_start, p, FORK))
+		return (pthread_mutex_unlock(p->left_fork) + 1);
+	pthread_mutex_lock(p->right_fork);
+	if (print_routine(time_now(p) - p->thread_start, p, FORK))
+		return (someone_died(time_now(p), p, 1, 1));
+	p->last_meal = time_now(p);
+	if (p->params->over)
+		return (someone_died(0, p, 1, 0));
+	ft_usleep(p->params->time2eat);
+	if (p->params->over)
+		return (1);
+	if (print_routine(time_now(p) - p->thread_start, p, EAT))
+		return (someone_died(0, p, 1, 0));
+	p->iter_num++;
+	pthread_mutex_unlock(p->left_fork);
+	pthread_mutex_unlock(p->right_fork);
+	return (0);
+}
+
+void	*thread_routine(void *job)
+{
+	int		starved;
+	t_philo	*philo;
+
+	philo = (t_philo *)job;
+	if (philo->id & 1)
+		ft_usleep(50);
+	philo->thread_start = philo->params->start;
+	philo->last_meal = time_now(philo);
+	while (!philo->dead && !philo->params->over)
+	{
+		if (!starved && !philo->params->over)
+			starved = ft_eat(philo);
+		if (!starved && !philo->params->over)
+			starved = ft_sleep_and_think(philo);
+		if (philo->iter_num == philo->params->max_iter)
+			break ;
+	}
+	return (NULL);
+}
